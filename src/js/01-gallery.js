4063ce229884a25d8de39fb7da52798bc4814650
Salnikova-Anna <anna_val@ukr.net>
2023-07-13 12:26:58 +0300

    task-03

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,20 @@
+import SimpleLightbox from 'simplelightbox';
 // Add imports above this line
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const galleryMarkUp = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+  <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`;
+  })
+  .join('');
+
+galleryRef.innerHTML = galleryMarkUp;
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('input[type="email"]'),
+  textarea: document.querySelector('textarea[name="message"]'),
+};
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const storageValue = {};
+
+populateFormFields();
+
+refs.form.addEventListener('input', throttle(onFormInputClick, 500));
+refs.form.addEventListener('submit', onButtonSubmitClick);
+
+function onFormInputClick(event) {
+  event.preventDefault();
+
+  if (event.target.tagName === 'INPUT') {
+    storageValue.email = event.target.value;
+  } else if (event.target.tagName === 'TEXTAREA') {
+    storageValue.message = event.target.value;
+  }
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(storageValue));
+}
+
+function populateFormFields() {
+  const storageData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (storageData) {
+    refs.input.value = storageData.email;
+    refs.textarea.value = storageData.message;
+  }
+
+  //   refs.input.value = storageData.email ? storageData.email : '';
+  //   refs.textarea.value = storageData.message ? storageData.message : '';
+}
+
+function onButtonSubmitClick(event) {
+  event.preventDefault();
+
+  event.target.reset();
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  localStorage.removeItem(STORAGE_KEY);
+}
